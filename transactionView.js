46f372c4b1ec9fe3cbbddf78c93c58dfd76b5aae
Kanstantsin <kostya.zvankovich@gmail.com>
2019-12-03 16:06:26 +0100

    Update transactionView.js

--- transactionView.js
+++ transactionView.js
@@ -1,155 +1,93 @@
 db.transactionView.drop()
 db.runCommand({
-  create: "transactionView",
-  viewOn: "Transactions",
-  pipeline: [
-    { 
-       $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1} 
-    },
-    {
-      $group:
-      { _id: 
+    create: "transactionView",
+    viewOn: "Transactions",
+    pipeline: [
         {
-          clientId: "$clientId",
-          accountId: "$accountId",
-          bookingDate: "$bookingDate",
-//                bookingDateClosingBalance: "$bookingDateClosingBalance"
+            $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
         },
-        bookingDateClosingBalance: { $last: "$bookingDateClosingBalance"}
-      }
-    },
-    {
-      $project:
-      {
-        clientId: "$_id.clientId",
-	accountId: "$_id.accountId",
-        bookingDate:
         {
-          $cond: [ {
-            $gte:
-            [
-              "$_id.bookingDate",
-              {
-                $let:
+            $group:
+            {
+                _id:
                 {
-                  vars: { startDate: { $subtract: [new ISODate(), 1209600000]}},
-                    in: "$$startDate"
-                }
-              }
-            ]
-          }, "$_id.bookingDate", { $subtract: [new ISODate(), 1209600000]} ]
+                    clientId: "$clientId",
+                    accountId: "$accountId",
+                    bookingDate: "$bookingDate"
+                },
+                lastBookingDate: { $last: "$bookingDate"},
+                bookingDateClosingBalance: { $last: "$bookingDateClosingBalance"}
+            }
         },
-        bookingDateClosingBalance: "$bookingDateClosingBalance"
-      }
-    },
-    // { 
-    //   $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1, "_id.bookingDate": 1} 
-    // },
-    // {
-    //   $group:
-    //   { 
-    //     _id: 
-    //     {
-    //       clientId: "$clientId",
-    //       accountId: "$accountId",
-    //       bookingDate: "$bookingDate"
-    //     },
-    //     bookingDateClosingBalances: 
-    //     { 
-    //       $cond: [ {
-    //         $gte:
-    //         [
-    //           "$_id.bookingDate",
-    //           {
-    //             $let:
-    //             {
-    //               vars: { startDate: { $subtract: [new ISODate(), 1209600000]}},
-    //                 in: "$$startDate"
-    //             }
-    //           }
-    //         ]
-    //       }, "$_id.bookingDateClosingBalance", { $last: "$_id.bookingDateClosingBalance"} ]
-    //     }
-    //   }
-    // },
-    {
-      $group:
-      { 
-        _id: 
         {
-          clientId: "$clientId",
-          accountId: "$accountId",
-          bookingDate: "$bookingDate"
+            $project:
+            {
+                clientId: "$_id.clientId",
+                accountId: "$_id.accountId",
+                bookingDate:
+                {
+                    $cond: [ {
+                        $gte:
+                            [
+                                "$_id.bookingDate",
+                                {
+                                    $let:
+                                        {
+                                            vars: { startDate: { $subtract: ["$lastBookingDate", 1209600000]}},
+                                            in: "$$startDate"
+                                        }
+                                }
+                            ]
+                    }, "$_id.bookingDate", { $subtract: ["$lastBookingDate", 1209600000]} ]
+                },
+                bookingDateClosingBalance: "$bookingDateClosingBalance"
+            }
         },
-        bookingDateClosingBalances: 
-        { 
-          $push: "$bookingDateClosingBalance"
+        {
+            $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1, "_id.bookingDate": 1}
+        },
+        {
+            $group:
+            {
+                _id:
+                {
+                    clientId: "$clientId",
+                    accountId: "$accountId",
+                    bookingDate: "$bookingDate"
+                },
+                bookingDateClosingBalance:
+                {
+                    $last: "$bookingDateClosingBalance"
+                }
+            }
+        },
+        {
+            $sort:{ "_id.clientId": 1, "_id.accountId": 1, "_id.bookingDate": 1}
+        },
+        {
+            $group:
+            {
+                _id:
+                {
+                    clientId: "$_id.clientId",
+                    bookingDate: "$_id.bookingDate"
+                },
+                bookingDateClosingBalance:
+                {
+                    $sum: "$bookingDateClosingBalance"
+                }
+            }
+        },
+        {
+            $sort:{ "_id.clientId": 1, "_id.bookingDate": 1}
+        },
+        {
+            $project:
+            {
+                _id: 0,
+                clientId: "$_id.clientId",
+                bookingDate: "$_id.bookingDate",
+                bookingDateClosingBalance: "$bookingDateClosingBalance"
+            }
         }
-      }
-    },
-    {
-      $project:
-      {
-        _id: 0,
-        clientId: "$_id.clientId",
-	accountId: "$_id.accountId",
-        bookingDate: "$_id.bookingDate",
-        bookingDateClosingBalances: "$bookingDateClosingBalances"
-      }
-    },
-    { 
-      $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1} 
-    },
-    // {
-    //   $addFields:
-    //   {
-    //     rangeDate:
-    //     {
-    //       $map:
-    //       { 
-    //         input:{$range:[0, 15*1000*60*60*24, 1000*60*60*24]},
-    //           in:{$subtract:[new ISODate(), "$$this"]}
-    //       }
-    //     }
-    //   }
-    // },
-    // { $unwind: "$rangeDate"},
-    // {
-    //   $group:
-    //   {
-    //     _id: 
-    //     {
-    //       clientId: "$clientId",
-    //       accountId: "$accountId",
-    //       rangeDate: "$rangeDate"
-    //     },
-    //     bookingDateClosingBalance: { $push: "$bookingDateClosingBalance"}
-    //   }
-    // },
-    // { $sort:{ "_id.clientId": 1, "_id.accountId": 1, "_id.rangeDate": 1} },
-    // {
-    //   $project: {
-    //     _id: 0,
-    //     rangeDate: "$_id.rangeDate",
-    //     clientId: "$_id.clientId",
-    //     accountId: "$_id.accountId",
-    //     bookingDateClosingBalance: "$bookingDateClosingBalance"
-    //     // bookingDateClosingBalance:
-    //     // {
-    //     //   $cond: [ {
-    //     //     $gte:
-    //     //     [
-    //     //       "$_id.bookingDate",
-    //     //       {
-    //     //         $let:
-    //     //         {
-    //     //           vars: { startDate: { $subtract: [new ISODate(), 1209600000]}},
-    //     //             in: "$$startDate"
-    //     //         }
-    //     //       }
-    //     //     ]
-    //     //   }, "$bookingDateClosingBalance", { $last: "$bookingDateClosingBalance"} ]
-    //     // }
-    //   }
-    //    }
 ]});
