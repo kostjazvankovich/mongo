27667dac3ba92848fd61e9b2f42818b36816f1a0
Kanstantsin <kostya.zvankovich@gmail.com>
2019-12-04 14:29:51 +0100

    create lastActiveTransactionView

---
+++ lastActiveTransactionView.js
@@ -0,0 +1,34 @@
+db.lastActiveTransactionView.drop()
+db.runCommand({
+    create: "lastActiveTransactionView",
+    viewOn: "Transactions",
+    pipeline: [
+        {
+            $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
+        },
+        {
+            $group:
+                {
+                    _id:
+                    {
+                        clientId: "$clientId",
+                        accountId: "$accountId",
+                    },
+                    lastBookingDate: { $last: "$bookingDate"},
+                    bookingDateClosingBalance: { $last: "$bookingDateClosingBalance"}
+                }
+        },
+        {
+            $project:
+            {
+                _id: 0,
+                clientId: "$_id.clientId",
+                accountId: "$_id.accountId",
+                lastBookingDate: "$lastBookingDate",
+                bookingDateClosingBalance: "$bookingDateClosingBalance"
+            }
+        },
+        {
+            $sort:{ "clientId": 1, "accountId": 1, "lastBookingDate": 1}
+        }
+    ]});
