4478e4288082d92525b208421cc15f43cdb4a35b
Kanstantsin <kostya.zvankovich@gmail.com>
2019-12-04 14:29:02 +0100

    update transactionView

--- transactionView.js
+++ transactionView.js
@@ -3,37 +3,66 @@ db.runCommand({
     create: "transactionView",
     viewOn: "Transactions",
     pipeline: [
-      { 
-        $facet: 
         {
-          "lastActiveTransactions" : [
+            $lookup:
             {
-              $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
-            },
-            {
-              $group:
-              {
-                _id:
+                from: "lastActiveTransactionView",
+                let: { bookingDate: "$bookingDate", accountId: "$accountId", clientId: "$clientId" },
+                pipeline: [
+                    { $match:
+                        { $expr:
+                            {  $and: [
+                                { $eq: [ "$accountId",  "$$accountId" ] },
+                                { $eq: [ "$clientId",  "$$clientId" ] },
+                                {
+                                    $gte:
+                                        [
+                                            "$$bookingDate",
+                                            {
+                                                $let:
+                                                {
+                                                    vars:
+                                                        {
+                                                            startDate: {$subtract: ["$lastBookingDate", 1209600000]}
+                                                        }, in: "$$startDate"
+                                                }
+                                            }
+                                        ]
+                                }]
+                            }
+                        }
+                    },
+                    { $project: { _id: 0 } }
+                ],
+                as: "activeTransactions"
+            }
+        },
+        {
+            $unwind: "$activeTransactions"
+        },
+        {
+            $group:
                 {
-                  clientId: "$clientId",
-                  accountId: "$accountId",
-                },
-                lastBookingDate: { $last: "$bookingDate"},
-                bookingDateClosingBalance: { $last: "$bookingDateClosingBalance"}
-              }
-            },
+                    _id:
+                    {
+                        clientId: "$clientId",
+                        accountId: "$accountId",
+                        bookingDate: "$bookingDate",
+                        bookingDateClosingBalance: "$bookingDateClosingBalance"
+                    },
+                }
+        },
+        {
+            $project:
             {
-              $project:
-              {
                 _id: 0,
-                bookingDate: "$lastBookingDate",
                 clientId: "$_id.clientId",
                 accountId: "$_id.accountId",
-                bookingDateClosingBalance: "$bookingDateClosingBalance"
-              }
-            },
-            {
-              $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
-            }]
-        }}
+                bookingDate: "$_id.bookingDate",
+                bookingDateClosingBalance: "$_id.bookingDateClosingBalance"
+            }
+        },
+        {
+            $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
+        }
 ]});
