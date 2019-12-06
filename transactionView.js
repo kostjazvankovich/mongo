53ba7aaa41aeb1b379bc357bb769fd620cf623a3
Kanstantsin <kostya.zvankovich@gmail.com>
2019-12-06 16:19:58 +0100

    Update transactionView.js

--- transactionView.js
+++ transactionView.js
@@ -13,6 +13,7 @@ db.runCommand({
                                 { $expr:
                                         {  $and: [
                                                 { $eq: [ "$clientId",  "$$clientId" ] },
+                                                { $eq: [ "$accountId",  "$$accountId" ] },
                                                 {
                                                     $gte:
                                                         [
@@ -53,68 +54,127 @@ db.runCommand({
         },
         {
             $project:
-            {
-                _id: 0,
-                clientId: "$_id.clientId",
-                accountId: "$_id.accountId",
-                bookingDate: "$_id.bookingDate",
-                bookingDateClosingBalance: "$_id.bookingDateClosingBalance"
-            }
+                {
+                    _id: 0,
+                    clientId: "$_id.clientId",
+                    accountId: "$_id.accountId",
+                    bookingDate: "$_id.bookingDate",
+                    bookingDateClosingBalance: "$_id.bookingDateClosingBalance"
+                }
         },
         {
             $sort:{ "clientId": 1, "accountId": 1, "bookingDate": 1}
         },
         {
             $group:
-            {
-                _id:
                 {
-                    clientId: "$clientId",
-                    accountId: "$accountId"
-                },
-                startDate: { $first: "$bookingDate"},
-                endDate: { $last: "$bookingDate"},
-                bookingBalances: { $push: { bookingDate: "$bookingDate", bookingDateClosingBalance: "$bookingDateClosingBalance" }}
-            }
+                    _id:
+                    {
+                        clientId: "$clientId",
+                        accountId: "$accountId"
+                    },
+                    startDate: { $first: "$bookingDate"},
+                    endDate: { $last: "$bookingDate"},
+                    bookingBalances: { $push: { bookingDate: "$bookingDate", bookingDateClosingBalance: "$bookingDateClosingBalance" }}
+                }
         },
         {
             $project:
+                {
+                    _id: 0,
+                    clientId: "$_id.clientId",
+                    accountId: "$_id.accountId",
+                    startDate: "$startDate",
+                    endDate: "$endDate",
+                    bookingBalances: "$bookingBalances"
+                }
+        },
+        {
+            $facet:
             {
-                _id: 0,
-                clientId: "$_id.clientId",
-                accountId: "$_id.accountId",
-                startDate: "$startDate",
-                endDate: "$endDate",
-                bookingBalances: "$bookingBalances"
+                "rangeTransactions": [
+                    {
+                        $sort: { "endDate": -1 }
+                    },
+                    { $limit: 1 },
+                    {
+                        $project:
+                        {
+                            endBookingDate: "$endDate"
+                        }
+                    },
+                    {
+                        $addFields:
+                        {
+                            rangeDate:
+                             {
+                               $map:
+                               {
+                                 input:{$range:[0, 15*1000*60*60*24, 1000*60*60*24]},
+                                   in:{ $subtract:["$endBookingDate", "$$this"]}
+                               }
+                             }
+                       }
+                    },
+                    { $unwind: "$rangeDate"},
+                    {
+                        $project:
+                        {
+                            bookingDate: "$rangeDate"
+                        }
+                    },
+                    {
+                        $sort:{ "bookingDate": 1}
+                    },
+                ],
+                "lastTransactions": [
+                    {
+                        $project:
+                        {
+                            clientId: "$clientId",
+                            accountId: "$accountId",
+                            startBookingDate: "$startDate",
+                            endBookingDate: "$endDate",
+                            bookingBalances: "$bookingBalances"
+                        }
+                    }
+                ]
             }
         },
-      {
-        $facet: 
+        { $unwind: "$rangeTransactions" },
+        { $unwind: "$lastTransactions" },
         {
-          "lastTransaction": [
-            {
-              $sort: { "endDate": -1 }
-            },
-            { $limit: 1 },
-            { 
-              $project: 
-              {
-                bookingDate: "$endDate",
-                startDate: "$startDate"
-              }
-            
-            }
-          ],
-          "lastTransactions": [
             $project:
             {
-                _id: 0,
-                clientId: "$_id.clientId",
-                accountId: "$_id.accountId",
-                startDate: "$startDate",
-                endDate: "$endDate",
-                bookingBalances: "$bookingBalances"
-          ]
-        }
-      }
-    ]});
+                bookingDate: "$rangeTransactions.bookingDate",
+                startBookingDate: "$lastTransactions.startBookingDate",
+                endBookingDate: "$lastTransactions.endBookingDate",
+                bookingDateClosingBalance:
+                {
+                    $cond: [
+                        {
+                            $gte:  ["$rangeTransactions.bookingDate", "$lastTransactions.startBookingDate"],
+                        },
+                        {
+                            $arrayElemAt: [
+                                {
+                                    $filter: {
+                                    "input": "$lastTransactions.bookingBalances",
+                                    "as": "balance",
+                                    "cond": {
+                                        "$and": [
+                                            {"$lte": ["$rangeTransactions.bookingDate", "$$balance.bookingDate"]}
+                                        ]
+                                    }}
+                                }, -1
+                            ]
+                        },
+                        { $arrayElemAt: [ "$lastTransactions.bookingBalances", 0 ] }
+                    ]
+                }
+            }
+        },
+        {
+            $sort:{ "bookingDate": 1}
+        },
+]});
